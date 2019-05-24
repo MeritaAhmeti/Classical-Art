ec90020073f9e50e4d23ff902c7cac6d48a35aa8
MeritaAhmeti <44531649+MeritaAhmeti@users.noreply.github.com>
2019-05-24 22:31:30 +0200

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,147 @@
+<html manifest="manifest.appcache">
+    <head>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1">
+        <link rel="stylesheet" href="CSS/gallery.css">
+        <link rel="stylesheet" href="CSS/dropdown.css">
+        <link rel="stylesheet" href="CSS/header.css">
+
+        <title>Gallery</title>
+    </head>
+    <body>
+        <div class="top-bar">
+            <div id="rectangle"></div>
+            <div id="contact"><p>Tel: 11111 2222222222 | Mail: <a href="mailto:info@classicalart.com"/>info@classicalart.com</p></a></div>
+            <div class="search-bar">
+            <form>
+                <input type="text" placeholder="Search" name="Search">
+                <img src="Fotot/Search-button.png"/>
+            </form>
+            </div>
+        </div>
+        <header>
+            <div class="logo">
+                <h1>Classical Art</h1>
+                <h4>Free PSD Website Template</h4>
+            </div>
+            <div class="menu">
+                <ul class="nav">
+                    <li><a id="v" href="index.php">HOME</a></li>
+                    <li><a id="v" href="forma.php">BUY TICKETS</a></li>
+                    <li><a id="v" href="curiosities.php">CURIOSITIES</a></li>
+                    <li><a id="v" href="#">DROPDOWN</a>
+                        <ul class="sub-menu">
+                            <li><a href="puzzle.html">PUZZLE</a></li>
+                            <li><a href="tabela/tabela.html">ARTISTS</a></li>
+
+                        </ul>    
+                    </li>
+                    <li><a href="gallery.php" style="color: #1194B2">GALLERY</a></li>
+                </ul>
+            </div>
+        </header>
+            <div class="content1">
+            	<span class="fadingline2"></span>
+                <div id="desc">
+                    <h1>The World's Best Cities for Arts and Culture</h1>
+                    <p>We asked our readers to rate their favorite cities in the world for arts and culture. These cities are centers of music and dance, museums, and theater. They are where iconic design and classical concertos were born, and where the latest in creative experimentation is happening seemingly on every corner. Here are the best cities in the world for soaking in culture and finding inspiration in the arts, starting with the top spot.</p>
+            <span class="fadingline2"></span>
+                    <p>Share:</p><br>
+                    <a href="https://outlook.live.com/owa/"><img src="Fotot/email.png" alt="Email"/></a>
+                    <a href="https://web.facebook.com/?_rdc=1&_rdr"><img src="Fotot/facebook.png" alt="Facebook"/></a>
+                    <a href="https://twitter.com/"><img src="Fotot/twitter.png" alt="Twitter"/></a>
+                </div>
+            </div>
+
+
+          <div class="content2">
+          	<div id="desc1">
+          		<h3>For more information click the images!</h3>
+          	</div>
+          </div>
+
+               <div class="container">
+                <div class="gallery">
+                 <a target="_blank" href="qytetet/Florence.html">
+                 <img src="Fotot/Florence.jpg" alt="Florence" width="300" height="300" >
+                 </a>
+                 <div class="pershkrimi">Florence,Italy</div>
+                 </div>
+                 
+
+                
+                 <div class="gallery">
+                 <a target="_blank" href="qytetet/Rome.html">
+                 <img src="Fotot/Rome.jpg" alt="Rome" width="300" height="300" >
+                 </a>
+                 <div class="pershkrimi">Rome,Italy</div>
+                 </div>
+                
+
+                <div class="gallery">
+                <a target="_blank" href="qytetet/Paris.html">
+                <img src="Fotot/Paris.jpg" alt="Paris" width="300" height="300" >
+                </a>
+                <div class="pershkrimi">Paris,France</div>
+                </div>
+              
+
+                <div class="container">
+                 <div class="gallery">
+                 <a target="_blank" href="qytetet/Athens.html">
+                 <img src="Fotot/Athens.jpg" alt="Athens" width="300" height="300" >
+                 </a>
+                 <div class="pershkrimi">Athens,Greece</div>
+                 </div>
+                
+
+                
+                <div class="gallery">
+                <a target="_blank" href="qytetet/Barcelona.html">
+                <img src="Fotot/Barcelona.jpg" alt="Barcelona" width="300" height="300" >
+                </a>
+                <div class="pershkrimi">Barcelona,Spain</div>
+                </div>
+              
+
+                
+                <div class="gallery">
+                <a target="_blank" href="qytetet/Istanbul.html">
+                <img src="Fotot/Istanbul.jpg" alt="Istanbul" width="300" height="300" >
+                </a>
+                <div class="pershkrimi">Istanbul,Turkey</div>
+                </div>
+             
+                <div class="gallery">
+                <a target="_blank" href="qytetet/Santa Fe.html">
+                <img src="Fotot/Santa Fe.jpg" alt="Santa Fe" width="300" height="300" >
+                </a>
+                <div class="pershkrimi">Santa Fe,New Mexico</div>
+                </div>
+                
+
+                <div class="gallery">
+                <a target="_blank" href="qytetet/Venice.html">
+                <img src="Fotot/Venice.jpg" alt="Venice" width="300" height="300" >
+                </a>
+                <div class="pershkrimi">Venice,Italy</div>
+                </div>
+
+
+                <div class="gallery">
+                <a target="_blank" href="qytetet/NewYork.html">
+                <img src="Fotot/NewYork.jpg" alt="New York" width="300" height="300" >
+                </a>
+                <div class="pershkrimi">New York,USA</div>
+                </div>
+            </div>
+        </div>
+<footer>
+	 <div class="copyright">
+                <p>Copyright &copy; 2012 Domain Name - All rights reserved <span class="template">Template by OS templates</span></p>
+            </div>
+        </footer>
+
+</body>
+</html>
