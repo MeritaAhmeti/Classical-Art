f1e9a207a079aac38dbbe6236b05462ee81929bd
Erblini <39314847+Erblini@users.noreply.github.com>
2019-05-24 22:59:20 +0200

    initial commit

---
+++ curiosities.php
@@ -0,0 +1,107 @@
+<html manifest="manifest.appcache">
+    <head>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1">
+        <link rel="stylesheet" href="CSS/header.css">
+        <link rel="stylesheet" href="CSS/curiosities.css">
+        <link rel="stylesheet" href="CSS/dropdown.css">
+
+        <title>Classical-Art</title>
+    
+    </head>
+    <body>
+        <script src="canvas.js"></script>
+
+        <div class="top-bar">
+            <div id="rectangle"></div>
+            <div id="contact"><p>Tel: 11111 2222222222 | Mail: <a href="mailto:info@classicalart.com"/>info@classicalart.com</p></a></div>
+            <div class="search-bar">
+            <form>
+                <input type="text" placeholder="Search" name="Search">
+                <img src="Fotot/Search-button.png"/>
+            </form>
+            </div>
+        </div>
+        <header>
+            <div class="logo">
+                <h1>Classical Art</h1>
+                <h4>Free PSD Website Template</h4>
+            </div>
+           <div class="menu">
+                <ul class="nav">
+                    <li><a id="v" href="index.php" >HOME</a></li>
+                    <li><a id="v" href="forma.php">BUY TICKETS</a></li>
+                    <li><a id="v" href="curiosities.php" style="color: #1194B2">CURIOSITIES</a></li>
+                    <li><a id="v" href="#">DROPDOWN</a>
+                        <ul class="sub-menu">
+                            <li><a href="puzzle.html">PUZZLE</a></li>
+                            <li><a href="tabela/tabela.html">ARTISTS</a></li>
+
+                        </ul>    
+                    </li>
+                    <li><a href="gallery.php">GALLERY</a></li>
+                </ul>
+            </div>
+        </header>
+
+        <div class="titulli">
+            <h1>10 Curiosities About Art </h1>
+        </div>
+        <div class="art1">
+
+            <img src="Fotot/art1.png" width="800" height="500">
+            <h2>1. Mona Lisa</h2>
+            <p class="mona">While some that claim <mark>Leonardo da Vinci</mark>’s most famous painting is a self-portrait of the artist himself in drag, research has concluded it is likely a portrait of a woman named Lisa Gherardini, a member of a prominent Florentine family and wife of a wealthy silk merchant. Leonardo’s father allegedly knew Gherardini’s father very well, and the painting was possibly commissioned by him.</p>
+
+            <h2>2. The Last Supper</h2>
+            <img src="Fotot/art2.jpg" width="600" height="340">
+            <p class="mona">
+                Da Vinci’s other most famous work—which can be seen in the Convent of Santa Maria delle Grazie in <mark>Milan</mark>, Italy—originally included Jesus’ feet. But in 1652, while installing a doorway in the refectory where the painting is on view, builders cut into the bottom-center of the mural, lopping off Jesus’ feet.
+            </p>
+
+            <h2>3. The Starry Night</h2>
+            <img src="Fotot/art3.jpg" width="600" height="400">
+            <p class="mona">
+                The small town depicted in Vincent van Gogh’s The Starry Night is Saint-Rémy-de-Provence in the south of France. Van Gogh painted the work while he was a patient at the Saint-Paul-de-Mausole, a psychiatric hospital in Saint-Rémy. Presently, the hospital has a wing named after the painter.
+                <h2>4. Michelangelo's David</h2>
+                <img src="Fotot/art4.png" width="600" height="400">
+                <p class="mona">
+                    The marble slab that was eventually turned into the sculpture of David by Michelangelo in 1504 was cut 43 years earlier for an artist named Agostino di Duccio, who planned to turn it into a statue of Hercules. Di Duccio abandoned his sculpture, which was originally to be installed in a Florentine cathedral, and the marble was unused for 10 years until another sculptor, named <mark>Antonio Rossellino</mark>, decided to work with it. Rossellino also abandoned his work because he found marble too difficult to sculpt, and eventually Michelangelo began work on his sculpture in 1501.
+                </p>
+
+            <h2>5. The Creation of Adam</h2>
+            <img src="Fotot/art5.jpg" width="600" height="300">
+            <p class="mona">Michelangelo painted the fresco ceiling of the Sistine Chapel—including the most famous panel called “The Creation of Adam,” which depicts God giving life to the first man—entirely standing up. The artist invented a series of scaffolds specially designed to attach to the chapel walls with brackets so he and his assistants could be close enough to the ceiling to reach above their heads to work and paint.</p>
+
+            <h2>6. The Scream</h2>
+            <img src="Fotot/art6.png" width="600" height="400">
+            <p class="mona">There are technically five separate versions of Expressionist artist <mark>Edvard Munch</mark>’s most famous work, The Scream. The first two, from 1893 and created with tempera and crayon on cardboard, are located in the National Gallery in Oslo and the Munch Museum, respectively. A privately owned third version created in 1895 with pastels recently sold for nearly $120 million at auction. Yet another version from 1895 is a black and white lithograph. A final version, done in 1910 by Munch due to the popularity of the previous incarnations, is also held in the Munch Museum, and it made headlines in recent years for being stolen in 2004 and recovered in 2006.</p>
+
+            <h2>7. The Thinker</h2>
+            <img src="Fotot/art7.png" width="600" height="400">
+            <p class="mona">Though there are now dozens of casts of <mark>Auguste Rodin</mark>’s famous sculpture The Thinker around the world, it had a much smaller origin. Rodin originally created a 70cm version in 1880 as the central component to a bigger sculptural work called “The Gates of Hell.” Inspired by Dante’s Inferno, the piece—first called The Poet—was conceived as a representation of Dante himself. The re-dubbed sculpture was exhibited on its own in 1888, then was enlarged to the depiction we know it today in 1904.</p>
+
+            <h2>8. Girl with a Pearl Earring</h2>
+            <img src="Fotot/art8.png" width="600" height="400">
+            <p class="mona">Much like the Mona Lisa, the subject of <mark>Johannes Vermeer</mark>’s Girl with a Pearl Earring has been highly contested—but for the most likely candidate, Vermeer didn’t have to look far. The model for his painting is thought to be his daughter Maria.</p>
+
+            <h2>9. American Gothic</h2>
+            <img src="Fotot/art9.png" width="600" height="400">
+            <p class="mona">Another famous painting with interesting models is <mark>Grant Wood</mark>’s American Gothic, which can be seen on view in the collection of the Art Institute of Chicago. To depict—for better or worse—the ideals of rural America, Wood wanted to use his mother, Hattie, as a model for his painting. Wood determined that standing for so long would be far too exhausting for his mother, so he had his sister wear his mother’s apron and pin while posing. For the male subject in the painting, Wood used his 62-year-old dentist.</p>
+
+            <h2>10. Nighthawks</h2>
+            <img src="Fotot/art10.jpg" width="600" height="350">
+            <p class="mona">Another painting in the collection of the Art Institute of Chicago is Edward Hopper’s Nighthawks. Hopper allegedly based the painting on a diner that was located in New York City’s Greenwich Village in an area where Greenwich Street meets 11th Street and 7th Avenue called Mulry Square. But he actually based the painting on an all-night coffee stand. “I simplified the scene a great deal and made the restaurant bigger,” he said. “Unconsciously, probably, I was painting the loneliness of a large city.”</p>
+            
+        </div>
+        <div class="copyright">
+                <p>Copyright &copy; 2012 Domain Name - All rights reserved <span class="template">Template by OS templates</span></p>
+            </div>
+
+
+
+
+
+    </body>
+    </html>
---
+++ forma.php
@@ -0,0 +1,137 @@
+<html manifest="manifest.appcache">
+    <head>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1">
+        <link rel="stylesheet" href="CSS/forma.css">
+        <link rel="stylesheet" href="CSS/header.css">
+        <link rel="stylesheet" href="CSS/dropdown.css">
+        <link rel="stylesheet" href="CSS/footer.css">
+       
+        <title>Classical-Art</title>
+    
+    </head>
+    <body>
+        
+<!-- PJESA 1 : HEADER --> 
+        <div class="top-bar">
+            <div id="rectangle"></div>
+            <div id="contact"><p>Tel: 11111 2222222222 | Mail: <a href="mailto:info@classicalart.com"/>info@classicalart.com</p></a></div>
+            <div class="search-bar">
+            <form>
+                <input type="text" placeholder="Search" name="Search">
+                <img src="Fotot/Search-button.png"/>
+            </form>
+            </div>
+        </div>
+        <header>
+            <div class="logo">
+                <h1 class="klasik">Classical Art</h1>
+                <h4 class="free">Free PSD Website Template</h4>
+            </div>
+            <div class="menu">
+                <ul class="nav">
+                    <li><a id="v" href="index.php">HOME</a></li>
+                    <li><a id="v" href="#" style="color: #1194B2">BUY TICKETS</a></li>
+                    <li><a id="v" href="curiosities.php">CURIOSITIES</a></li>
+                    <li><a id="v" href="#">DROPDOWN</a>
+                        <ul class="sub-menu">
+                            <li><a href="puzzle.html">PUZZLE</a></li>
+                            <li><a href="tabela/tabela.html">ARTISTS</a></li>
+
+                        </ul>    
+                    </li>
+                    <li><a href="gallery.php">GALLERY</a></li>
+                </ul>
+            </div>
+        </header>
+        
+        
+<!-- PJESA 2 : FORMA -->
+<div class="mforma">
+    <h1>Register Here To Buy a GLEEN BROWN  Art Gallery Ticket</h1>  
+    <div class="reg">
+        <form id="reg" method="post" oninput="x.value=parseInt(tk.value)*parseInt(tp.value)">
+            <h2>Please fill out all required fields</h2>
+            
+            <label>First Name :</label><br>
+            <input type="text" name="name" placeholder="First Name" id="name" autofocus >
+            <br><br>
+            
+            <label>Last Name :</label><br>
+            <input type="text" name="name" placeholder="Last Name" id="name">
+            <br><br>
+            
+            <label>Date Of Birth :</label><br>
+            <input type="date" name="date" id="name">
+            <br><br>
+            
+            <label>Gender :</label><br>
+            <input type="radio" name="gender" value="male" id="ma" checked><span id="ma">Male</span> 
+            <input type="radio" name="gender" value="female" id="ma"><span id="ma">Female</span>
+            <input type="radio" name="gender" value="other" id="ma"><span id="ma">Other</span>
+            <br><br>
+            
+            
+            <label>How Many Tickets: </label><br>
+            <input type="number" name="number" min="1" max="5" id="tk" value="1"><strong id="tb">*</strong>
+            <input type="number" name="number" value="15" id="tp" disabled ><strong id="tb">$ =</strong>
+            <output name="x" for="tk tp" value="15" id="tb"></output><strong id="tb">$</strong>
+            <br><br><br>
+            
+            <label>What Are You Interested In: </label><br>
+            <input type="text" name="arttypes" list="art" id="name" placeholder="Select an art-style">
+              <datalist id="art" > 
+                <option>Abstract Art</option>
+                <option>Sculpture</option>
+                <option>Portrait</option>
+                <option>Art Movies</option>
+              </datalist>
+            <br><br>
+            
+            <label>Email :</label><br>
+            <input type="email" name="email" placeholder="E.g example@email.com" id="name" >
+            <br><br>
+            
+            <label>Password :</label><br>
+            <input type="password" name="pass" placeholder="Enter your pasword" id="name" >
+            <br><br>
+            
+            <label>Description :</label><br>
+            <textarea name="message" rows="10" cols="39" placeholder="Write down your expectations about this Gallery Art so we can keep it up to you"></textarea>
+            <br><br>
+            
+            <label>Select Your Payment</label><br>
+              <select name="continent" id="name">
+                <option>PayPal</option>
+                <option>Visa</option>
+                <option>GoogleWallet</option>
+                <option>Venmo</option>
+                <option>Dwolla</option>
+              </select>
+            <br><br>
+            
+            <label>Credit-Card Number :</label><br>
+            <input type="text" name="number" placeholder="Enter your CC number" id="name">
+            <br><br>
+            
+            <input type="checkbox" id="ch" required><span id="ch"> I agree all the terms & conditions</span>
+            <br><br>
+            
+            <input class="button" type="button" onclick="alert('Sorry ! Tickets are sold !')" value="Buy Tickets Now">
+
+            
+        </form>
+    </div>
+</div>        
+        
+        
+<!-- PJESA 3 : FOOTER , COPYRIGHT -->
+
+        <div class="copyright">
+                <p>Copyright &copy; 2012 Domain Name - All rights reserved <span class="template">Template by OS templates</span></p>
+            </div>
+
+
+</body>
+</html>
