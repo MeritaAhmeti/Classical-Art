b1baeac610702585a46ea00388bbb36f6bd9f9c2
Erblini <39314847+Erblini@users.noreply.github.com>
2019-05-31 01:06:19 +0200

    Pjesa e kodit qe nuk lejon mu bo order submit pa i mbush te gjitha fields

--- forma-orders.php
+++ forma-orders.php
@@ -31,13 +31,68 @@ $rez = "
    | \t Terms : " . $terms ;
 
  $DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
-		$fileop = fopen("$DOCUMENT_ROOT/Github/test/rezervimet.txt", 'ab');
+		$fileop = fopen("$DOCUMENT_ROOT/PI18_19_Gr16-master/rezervimet.txt", 'ab');
 		fwrite($fileop, $rez);
-        fclose($fileop);  
+        fclose($fileop);          
 
 
 }
 
+$msg = '';
+if(filter_has_var(INPUT_POST, 'buy-tickets'))
+{
+   $firstName = $_POST["firstname"];
+   $lastName = $_POST["lastname"];
+   $dateOfBirth = $_POST["date"];
+   $number = $_POST["number"];
+   $interested = $_POST["interested-in"];
+   $email = $_POST["email"];
+   $description = $_POST["message"];
+   $payment = $_POST["payment"];
+   $credit = $_POST["c-number"];
+   $terms = $_POST["terms"];
 
+   if(!empty($firstName) && !empty($lastName) &&!empty($dateOfBirth) &&!empty($number) &&!empty($interested) &&!empty($email) &&!empty($description) &&!empty($payment) &&!empty($credit)   &&!empty($terms))
+     { 
+
+      if(filter_var($email, FILTER_VALIDATE_EMAIL) === false)
+      {
+         $msg = 'Email is not valid';
+      }
+      else
+      {
+         $toEmail = "erblinberisha@hotmail.com";
+         $subject =" Message from : " . $firstName;
+         $body = '
+         <h2>New message ! </h2>
+         <h4>Name</h4><p>' . $firstName . '</p>
+         <h4>Email</h4><p>' .$email. '</p>';
+
+         $headers = "MIME-Version: 1.0"."\r\n";
+         $headers .= "Content-Type:text/html;charset=UTF-8"."\r\n";
+
+         $headers .= "From: ".$firstName. "<".$email.">"."\r\n";
+
+         if(mail($toEmail,$subject,$body,$headers))
+            {
+               //Emaili dergohet
+               $msg = 'Your email is sent!';
+            }
+            else
+            {
+               $msg = 'Your email is not sent!';
+            }
+         }
+
+      }
+
+         else
+      {
+         //Nese nuk jane te mbushura
+         $msg = 'Please fill in all fields!';
+      }
+
+
+      }
 
 ?>
